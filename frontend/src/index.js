a3a233e9cb060661498e67ec4e9caaf7591aeef2
Rajath-gatty <rajathgatty253@gmail.com>
2022-04-01 19:28:53 +0530

    file changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from "react-dom/client";
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(<App />);
