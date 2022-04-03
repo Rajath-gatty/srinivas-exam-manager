272908d1eaf90d022303d14769bbb71c0dfcb5ec
Rajath-gatty <rajathgatty253@gmail.com>
2022-04-03 14:26:16 +0530

    login form complete

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,4 +1,8 @@
+import {Link} from "react-router-dom";
+import { useState } from "react";
+
 import "./Login.css";
+import "../../App.css";
 import SrinivasLogo from "../../Assets/SrinivasLogo.svg";
 import LoginSvg from "../../Assets/undraw_login.svg";
 import LockIcon from "../../Assets/lock_icon.svg";
@@ -6,7 +10,17 @@ import UserIcon from "../../Assets/user_icon.svg";
 
 
 const Login = () => {
-console.log("login page")
+  const [emailfocus, setEmailFocus] = useState(false);
+  const [passfocus, setPassFocus] = useState(false);
+
+  const onEmailActive = () => setEmailFocus(true);
+  const onPassActive = () => setPassFocus(true);
+
+  const onEmailBlur = () => setEmailFocus(false);
+  const onPassBlur = () => setPassFocus(false);
+
+  const emailAct = emailfocus ? 'form-control active': 'form-control';
+  const passAct = passfocus ? 'form-control active': 'form-control';
   
   return (
     <div className="login-container">
@@ -24,22 +38,25 @@ console.log("login page")
       <div className="login-form">
         <h1 className="login-hdng">LOGIN</h1>
         <form>
-            <div className="form-controls">
-              <img src={UserIcon} alt="" className="login-svg" />
+            <div className={emailAct} >
+              <label className="login-label">Email</label>
               <div className="input-group">
-                <label className="login-label">Email</label>
-                <input type="text" />
+                <input type="text" onFocus={onEmailActive} onBlur={onEmailBlur} placeholder="example@gmail.com" />
+                <img src={UserIcon} alt="" className="login-svg" />
               </div>
             </div>
-            <div className="form-controls">
-              <img src={LockIcon} alt="" className="login-svg"/>
+            <div className={passAct}>
+              <label className="login-label label-pass">Password</label>
               <div className="input-group">
-              <label className="login-label">Password</label>
-              <input type="password"/>
+                <input type="text" onFocus={onPassActive}  onBlur={onPassBlur} placeholder="Password" />
+                <img src={LockIcon} alt="" className="login-svg" />
               </div>
             </div>
+            <div className="forgot-pass">
+              <Link to="#">Forgot Password ?</Link>
+            </div>
             <div className="form-controls">
-              <input type="submit" value="submit" className="login-submit btn"/>
+              <input type="submit" value="Login" className="login-submit btn"/>
             </div>
         </form>
       </div>
