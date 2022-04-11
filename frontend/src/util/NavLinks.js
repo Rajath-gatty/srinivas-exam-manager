026dd647b763a2a0728622e71a856787a4206405
Rajath-gatty <rajathgatty253@gmail.com>
2022-04-11 20:28:33 +0530

    navlink edit

--- frontend/src/util/NavLinks.js
+++ frontend/src/util/NavLinks.js
@@ -1,39 +1,39 @@
-import {Dashboard,Register,Courses} from"../Assets";
+import { Dashboard, Register, Courses } from "../Assets";
 
 export const NavLinks = [
-    {
-        role: "admin",
-        links: [
-            {
-                title: "Dashboard",
-                path: "/dashboard",
-                icon: Dashboard
-            },
-            {
-                title: "create",
-                path: "/create",
-                icon: Courses
-            },
-            {
-                title: "register",
-                path: "/register",
-                icon: Register
-            }
-        ]
-    },
-    {
-        role: "student",
-        links: [
-            {
-                title: "Dashboard",
-                path: "/dashboard",
-                icon: Dashboard,
-            },
-            {
-                title: "register",
-                path: "/register",
-                icon: Register
-            }
-        ]
-    }
-]
+  {
+    role: "admin",
+    links: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: Dashboard,
+      },
+      {
+        title: "Create",
+        path: "/create",
+        icon: Courses,
+      },
+      {
+        title: "Register",
+        path: "/register",
+        icon: Register,
+      },
+    ],
+  },
+  {
+    role: "student",
+    links: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: Dashboard,
+      },
+      {
+        title: "Register",
+        path: "/register",
+        icon: Register,
+      },
+    ],
+  },
+];
