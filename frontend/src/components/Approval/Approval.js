df4995a7d2d14b252ed89f21e0fa5cf860a7f2d2
Rajath-gatty <rajathgatty253@gmail.com>
2022-04-15 19:24:28 +0530

    approval page edit

--- frontend/src/components/Approval/Approval.js
+++ frontend/src/components/Approval/Approval.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import {Back} from "../../Assets";
+import {Link} from "react-router-dom";
+import {Back,Avatar,Tick,Plus} from "../../Assets";
+import "./Approval.css";
+
 const Approval = () => {
   return (
-    <div className="approve-main">
+    <div className="approval-main">
     <div className="back-btn">
         <img src={Back} alt="Back" />
-        <span>Back</span>
+        <span className="back-btn-text">Back</span>
     </div>
-    <h1>Student Approval</h1>
-    
+    <h1 className="approve-list-header">Student Approval</h1>
+    <table className="approve-list-wrapper">
+      <tr>
+        <th>Profile</th>
+        <th>Name</th>
+        <th>Course</th>
+        <th>Batch</th>
+        <th>Details</th>
+        <th>Approval</th>
+      </tr>
+        <tr className="approve-table-row">
+          <td><img className="approve-list-avatar" src={Avatar} alt="Avatar"/></td>
+          <td> Rajath</td>
+          <td>BCA</td>
+          <td>2019</td>
+          <td><Link to="/approve/student/54674567">view</Link></td>
+          <td>
+          <button>
+            <img src={Tick} alt="Tick" width={20} />
+            Approve
+          </button>
+          <button>
+          <img src={Plus} alt="Tick" width={30} />
+            Reject
+          </button>
+          </td>
+        </tr>
+    </table>
 </div>
   )
 }
