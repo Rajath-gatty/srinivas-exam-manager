2bb5bf94d15f58f58695298ca5e5b0222a5c7593
Rajath-gatty <rajathgatty253@gmail.com>
2022-04-20 15:44:03 +0530

    create course completed

--- frontend/src/components/Admin/Course/Create/Create.js
+++ frontend/src/components/Admin/Course/Create/Create.js
@@ -1,6 +1,6 @@
 import { useNavigate} from "react-router-dom";
 import {FiArrowLeft} from "react-icons/fi";
-import {AiOutlinePlus} from "react-icons/ai";
+import {AiOutlinePlus,AiOutlineClose,AiOutlineMinusCircle} from "react-icons/ai";
 import {TextField,FormControl,Select,MenuItem,InputLabel} from "@mui/material";
 import "./Create.css";
 
@@ -44,7 +44,64 @@ const Create = () => {
                             <span>New Sem</span>
                         </button>
                     </div>
+                    <div className="semester">
+                        <div className="semester-header">
+                            <h3 className="sem-text">SEM 1</h3>
+                            <AiOutlineMinusCircle size={20} color="var(--strong-red)"/>
+                        </div>
+                        <div className="subject-details-wrapper">
+                        <TextField
+                            label="Subject Name"
+                            variant="standard"
+                            size="small"
+                            className="subject-input"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Subject code"
+                            variant="standard"
+                            size="small"
+                            className="subject-input"
+                            fullWidth
+                        />
+                        <button className="add-subject-btn btn-outlined flex">
+                            <AiOutlinePlus size={15}/>
+                            <span>Add</span>
+                        </button>
+                        </div>
+                        <div className="subject-info-main">
+                            <div className="subject-info-wrapper flex">
+                                <div className="subject-info">
+                                    <h3>Data Stuctures using c++</h3>
+                                    <p>19BCASD45</p>
+                                </div>
+                                <AiOutlineClose color="var(--strong-red)"/>
+                            </div>
+                            <div className="subject-info-wrapper flex">
+                                <div className="subject-info">
+                                    <h3>Advanced Java Programming</h3>
+                                    <p>19BCASD45</p>
+                                </div>
+                                <AiOutlineClose color="var(--strong-red)"/>
+                            </div>
+                            <div className="subject-info-wrapper flex">
+                                <div className="subject-info">
+                                    <h3>C++</h3>
+                                    <p>19BCASD45</p>
+                                </div>
+                                <AiOutlineClose color="var(--strong-red)"/>
+                            </div>
+                            <div className="subject-info-wrapper flex">
+                                <div className="subject-info">
+                                    <h3>Data Stuctures Algorithms using java</h3>
+                                    <p>19BCASD45</p>
+                                </div>
+                                <AiOutlineClose color="var(--strong-red)"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <button className="btn course-submit-btn" type="submit">Submit</button>
             </form>
         </div>
        </div>
