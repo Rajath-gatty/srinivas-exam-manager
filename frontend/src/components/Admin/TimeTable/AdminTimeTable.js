1336adc9a74297b026084764674c90b00c3ded3b
Rajath-gatty <rajathgatty253@gmail.com>
2022-04-21 23:10:33 +0530

    added file upload in admin timetable

--- frontend/src/components/Admin/TimeTable/AdminTimeTable.js
+++ frontend/src/components/Admin/TimeTable/AdminTimeTable.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import "./AdminTimeTable.css";
 import {FiUpload} from "react-icons/fi";
 import {IoMdClose} from "react-icons/io";
+
+import PdfIcon from "../../../Assets/pdf-icon.png";
+import "./AdminTimeTable.css";
 import Modal from "../../UI/Modal";
 import AdminTimeTableList from "./AdminTimeTableList";
 
 const AdminTimeTable = () => {
     const [showModal, setShowModal] = useState(false);
+    const [files, setFiles] = useState([]);
 
     const hideModalHandler = () => {
         setShowModal(false);
@@ -14,6 +17,16 @@ const AdminTimeTable = () => {
 
     const showModalHandler = () => {
         setShowModal(true);
+    }
+
+    const handleFileInput = (e) => {
+        const file = e.target.files;
+        setFiles(prev => [...prev,...file]);
+    }
+
+    const removeUploadedPdf = (index) => {
+        const newArr = files.filter((_,i) => i !== index);
+        setFiles(newArr);
     }
 
     return(
@@ -51,16 +64,27 @@ const AdminTimeTable = () => {
                 <IoMdClose size={25} className="timetable-close-icon" onClick={hideModalHandler}/>
                 <h2 className="upload-timetable-hdng">Upload Timetable</h2>
                 <div className="upload-wrapper">
-                    <div className="admin-upload">
-                        <button className="btn-outlined timetable-upload-btn flex">
-                            <input type="file" accept="application/pdf" className="input-file" id="timetable" name="timetable" />
-                            <FiUpload size={20}/>
-                            <label htmlFor="timetable">Upload Pdf</label>
-                        </button>
-                        <button className="timetable-send-btn btn">Send</button>
-                    </div>
                     <div className="display-timetable-wrapper">
-
+                        {files.map((file,index) => {
+                            return <div key={index} className="upload-file-info">
+                                <div className="info-img">
+                                    <img src={PdfIcon} alt="" />
+                                </div>
+                                <div className="file-info">
+                                    <h4>{file.name}</h4>
+                                    <p>{file.size}</p>
+                                </div>
+                                <IoMdClose onClick={() => removeUploadedPdf(index)} className="close-icon"/>
+                            </div>
+                        })}
+                    </div>
+                    <div className="admin-upload">
+                        <label htmlFor="timetable" className="btn-outlined timetable-upload-btn flex">
+                            <input type="file" accept="application/pdf" className="input-file" id="timetable" name="timetable" onChange={(e) => handleFileInput(e)} multiple />
+                            <FiUpload size={20}/>
+                            <span >Choose Pdf</span>
+                        </label>
+                        <button className="timetable-send-btn btn">Upload</button>
                     </div>
                 </div>
             </div>
