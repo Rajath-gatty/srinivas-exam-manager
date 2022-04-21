85be76d8b30c3383848b0ff242033a488bfc03cf
ChiragChrg <chiruchirag2001@gmail.com>
2022-04-21 21:28:17 +0530

    Added Exam Application Page

--- frontend/src/components/Student/Application/ApplicationRegular.js
+++ frontend/src/components/Student/Application/ApplicationRegular.js
@@ -1,5 +1,74 @@
-const  ApplicationRegular = () => {
-     return <h1>Application Regular Page</h1>
-}
+import { Checkbox } from "@mui/material";
+import "./Application.css";
+
+const ApplicationRegular = () => {
+  return (
+    <div className="application-regular flex">
+      <h2>Current Semester</h2>
+      <div className="application-semester">SEM II</div>
+
+      <div className="application-form flex">
+        <div className="application-row header">
+          <span>Subject Name</span>
+          <span>Subject Code</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="HTML" disabled checked /> <span>HTML</span>
+          </div>
+          <span>19BCASD55</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="CSS" disabled checked /> <span>CSS</span>
+          </div>
+          <span>19BCASD56</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="React" disabled checked /> <span>React</span>
+          </div>
+          <span>19BCASD57</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="Node.js" disabled checked /> <span>Node.js</span>
+          </div>
+          <span>19BCASD58</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="OS" disabled checked /> <span>OS</span>
+          </div>
+          <span>19BCASD59</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="AI" disabled checked /> <span>AI</span>
+          </div>
+          <span>19BCASD60</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="SE" disabled checked /> <span>SE</span>
+          </div>
+          <span>19BCASD61</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="Java" disabled checked /> <span>Java</span>
+          </div>
+          <span>19BCASD62</span>
+        </div>
+      </div>
+
+      <div className="application-total">
+        <span>Total Amount</span> <span>Rs.1600</span>
+      </div>
+
+      <button className="application-submit">Apply</button>
+    </div>
+  );
+};
 
 export default ApplicationRegular;
--- frontend/src/components/Student/Application/ApplicationRepeater.js
+++ frontend/src/components/Student/Application/ApplicationRepeater.js
@@ -1,5 +1,138 @@
-const  ApplicationRepeater = () => {
-     return <h1>Application Repeater Page</h1>
-}
+import { useState, useEffect } from "react";
+import {
+  FormControl,
+  FormControlLabel,
+  Select,
+  MenuItem,
+  InputLabel,
+  Checkbox,
+} from "@mui/material";
+import "./Application.css";
+
+const ApplicationRepeater = () => {
+  const [selectedSemester, setSelectedSemester] = useState("");
+  const [selectedSubject, setSelectedSubject] = useState([]);
+
+  const AddSubject = (e) => {
+    const CheckFlag = e.target.checked;
+    const SubjectName = e.target.value;
+
+    if (CheckFlag) {
+      setSelectedSubject([...selectedSubject, SubjectName]);
+    } else {
+      setSelectedSubject(selectedSubject.filter((e) => e !== SubjectName));
+    }
+  };
+
+  const TotalFee = 200 * selectedSubject.length;
+
+  return (
+    <div className="application-repeater flex">
+      <h2>Choose Repeater Semester</h2>
+      <div className="application-selector flex">
+        <FormControl className="select-sem">
+          <InputLabel>Select Semester</InputLabel>
+          <Select
+            label="Department"
+            defaultValue=""
+            size="small"
+            onChange={(e) => {
+              setSelectedSemester(e.target.value);
+            }}
+          >
+            <MenuItem value="SEM I">I Semester</MenuItem>
+            <MenuItem value="SEM II">II Semester</MenuItem>
+            <MenuItem value="SEM III">III Semester</MenuItem>
+            <MenuItem value="SEM IV">IV Semester</MenuItem>
+            <MenuItem value="SEM V">V Semester</MenuItem>
+            <MenuItem value="SEM VI">VI Semester</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className="application-semester">
+        {selectedSemester ? selectedSemester : "Select Semester"}
+      </div>
+
+      <div className="application-form flex">
+        <div className="application-row header">
+          <span>Subject Name</span>
+          <span>Subject Code</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="HTML" onChange={AddSubject} /> <span>HTML</span>
+          </div>
+          <span>19BCASD55</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="CSS" onChange={AddSubject} /> <span>CSS</span>
+          </div>
+          <span>19BCASD56</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="React" onChange={AddSubject} /> <span>React</span>
+          </div>
+          <span>19BCASD57</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="Node.js" onChange={AddSubject} />
+            <span>Node.js</span>
+          </div>
+          <span>19BCASD58</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="OS" onChange={AddSubject} /> <span>OS</span>
+          </div>
+          <span>19BCASD59</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="AI" onChange={AddSubject} /> <span>AI</span>
+          </div>
+          <span>19BCASD60</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="SE" onChange={AddSubject} /> <span>SE</span>
+          </div>
+          <span>19BCASD61</span>
+        </div>
+        <div className="application-row">
+          <div className="subject-checkbox">
+            <Checkbox value="Java" onChange={AddSubject} /> <span>Java</span>
+          </div>
+          <span>19BCASD62</span>
+        </div>
+      </div>
+
+      {selectedSubject.length !== 0 ? (
+        <div className="selected-subjects flex">
+          <div className="selected-subject-header flex">
+            <span>Repeater Subjects</span>
+            <span>Fees</span>
+          </div>
+
+          {selectedSubject.map((subject) => {
+            return (
+              <div key={subject} className="selected-subject-list">
+                <span>{subject}</span>
+                <span>Rs.200</span>
+              </div>
+            );
+          })}
+
+          <div className="application-total">
+            <span>Total Amount</span> <span>Rs.{TotalFee}</span>
+          </div>
+          <button className="application-submit">Apply</button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
 export default ApplicationRepeater;
