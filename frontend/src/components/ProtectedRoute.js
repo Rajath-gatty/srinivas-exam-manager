e59e6af17885a51f9bbe71087e5d26e39b713851
ChiragChrg <chiruchirag2001@gmail.com>
2022-04-24 02:04:12 +0530

    Removed unused imports

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,15 +1,14 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import {useContextData} from "../hooks/useContextData";
+import { Outlet } from "react-router-dom";
+import { useContextData } from "../hooks/useContextData";
 import PageNotFound from "../pages/PageNotFound/PageNotFound";
 
 const ProtectedRoute = ({ allowedRole }) => {
-    const {role} = useContextData();
-    const location = useLocation();
-    return (
-           allowedRole.find(item => item===role)
-           ?<Outlet/>
-           :<PageNotFound/>
-    )
-}
+  const { role } = useContextData();
+  return allowedRole.find((item) => item === role) ? (
+    <Outlet />
+  ) : (
+    <PageNotFound />
+  );
+};
 
 export default ProtectedRoute;
