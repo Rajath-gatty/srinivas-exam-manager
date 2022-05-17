36d687821d319a1f32975cbdec450cef6f9c687b
siddharthkm121 <siddharthkm121@gmail.com>
2022-05-17 10:12:59 +0530

    added

--- frontend/src/components/Staff/Indent/IndentRepeater.js
+++ frontend/src/components/Staff/Indent/IndentRepeater.js
@@ -2,67 +2,9 @@ import "./Indent.css";
 import IndentRepeaterList from "./IndentRepeaterList";
 import FilterSearch from "../../UI/FilterSearch/FilterSearch";
 const IndentRepeater = () => {
-<<<<<<< HEAD
-     const departments = [
-          "BCA",
-          "BBA",
-          "BCOM",
-          "BHM",
-          "BPT"
-     ];
-     const semeseter = [
-          "sem1",
-          "sem2",
-          "sem3",
-          "sem4",
-     ]
      return (
           <div className="indent-main">
-               <div className="filter-wrapper">
-
-                    <div className="filter-eligibility">
-                         <FormControl className="SelectInput">
-                              <InputLabel>Filter by Semeseter</InputLabel>
-                              <Select
-                                   label="semester"
-                                   defaultValue=""
-                                   placeholder="Filter by semester"
-                                   size="small"
-                                   className="select-filter"
-                              >
-                                   {semeseter.map((opt) => (
-                                        <MenuItem key={opt} value={opt}>
-                                             {opt}
-                                        </MenuItem>
-                                   ))}
-                              </Select>
-                         </FormControl>
-                    </div>
-                    <div className="filter-by-course">
-                         <FormControl className="SelectInput">
-                              <InputLabel>Filter by Department</InputLabel>
-                              <Select
-                                   className="select-filter"
-                                   label="Department"
-                                   defaultValue=""
-                                   placeholder="Filter by Course"
-                                   size="small"
-                              >
-                                   {departments.map((opt) => (
-                                        <MenuItem key={opt} value={opt}>
-                                             {opt}
-                                        </MenuItem>
-                                   ))}
-                              </Select>
-                         </FormControl>
-
-                    </div>
-               </div>
-=======
-     return (
-          <div className="indent-main">
-               <FilterSearch search/>
->>>>>>> 42a830b15a74020c4be13d2176678acb0aa85b2c
+               <FilterSearch search />
                <div className="main-box">
                     <div className="main-header">
                          <h3>SEM1</h3>
