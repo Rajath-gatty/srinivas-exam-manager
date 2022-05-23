762fc60c1b483a3d63ef0a03a8604e0d5cef01db
ChiragChrg <chiruchirag2001@gmail.com>
2022-05-23 20:35:36 +0530

    Minor Adjustments

--- frontend/src/components/Staff/Indent/IndentRegular.js
+++ frontend/src/components/Staff/Indent/IndentRegular.js
@@ -1,31 +1,19 @@
 import "./Indent.css";
-import IndentRegularList from "./IndentRegularList";
+// import IndentRegularList from "./IndentRegularList";
 import FilterSearch from "../../UI/FilterSearch/FilterSearch";
 const IndentRegular = () => {
-    const departments = [
-        "BCA",
-        "BBA",
-        "BCOM",
-        "BHM",
-        "BPT"
-    ];
-    const semeseter = [
-        "sem1",
-        "sem2",
-        "sem3",
-        "sem4",
-    ]
-    return (
-        <div className="indent-main">
-            <FilterSearch search />
-            <div className="main-box">
-                <div className="main-header">
-                    <h3>SEM1</h3>
-                </div>
-            </div>
+  //   const departments = ["BCA", "BBA", "BCOM", "BHM", "BPT"];
+  //   const semeseter = ["sem1", "sem2", "sem3", "sem4"];
+  return (
+    <div className="indent-main">
+      <FilterSearch search />
+      <div className="main-box">
+        <div className="main-header">
+          <h3>SEM1</h3>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default IndentRegular;
-
