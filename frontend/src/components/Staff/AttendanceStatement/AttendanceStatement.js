1959e2e2c03d1b821d10e8c69aae414838603aab
siddharthkm121 <siddharthkm121@gmail.com>
2022-05-24 11:58:23 +0530

    attendance statement completed

--- frontend/src/components/Staff/AttendanceStatement/AttendanceStatement.js
+++ frontend/src/components/Staff/AttendanceStatement/AttendanceStatement.js
@@ -1,8 +1,35 @@
+import FilterSearch from "../../UI/FilterSearch/FilterSearch";
+import "./AttendanceStatement.css";
+const AttendanceStatement = () => {
 
-const  AttendanceStatement = () => {
-    return(
-        <h1>Attendance statement Page</h1>
-    ) 
+
+    return (
+        <div className="attendance-main">
+            <FilterSearch />
+            <div className="main-box">
+                <tr className="text-field">
+                    <td>Subject Name</td>
+                    <td className="input-row"><input type="text"></input></td>
+                </tr>
+                <tr className="text-field">
+                    <td>Subject Code</td>
+                    <td className="input-row"><input type="text"></input></td>
+                </tr>
+                <tr className="text-field">
+                    <td>Total No students</td>
+                    <td className="input-row"><input type="text"></input></td>
+                </tr>
+                <tr className="text-field">
+                    <td>No of absenties</td>
+                    <td className="input-row"><input type="text"></input></td>
+                </tr>
+                <div className="button">
+                    <h3>Submit</h3>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default AttendanceStatement;
