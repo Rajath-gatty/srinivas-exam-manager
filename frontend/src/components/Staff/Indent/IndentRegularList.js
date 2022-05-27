caa6622569e9de78a057ab17602500afec59ee80
siddharthkm121 <siddharthkm121@gmail.com>
2022-05-27 14:16:52 +0530

    small changes done

--- frontend/src/components/Staff/Indent/IndentRegularList.js
+++ frontend/src/components/Staff/Indent/IndentRegularList.js
@@ -3,7 +3,7 @@ const IndentRegularList = () => {
         <tr className="indent-table-row">
             <td>HTML</td>
             <td>3SU19SA050</td>
-            <td>98</td>
+            <td><input type="text" font size="40px"></input></td>
             <td>65</td>
         </tr>
 
--- frontend/src/components/Staff/Indent/IndentRepeaterList.js
+++ frontend/src/components/Staff/Indent/IndentRepeaterList.js
@@ -3,7 +3,7 @@ const IndentRepeaterList = () => {
         <tr className="indent-table-row">
             <td>Software Engineering</td>
             <td>3SU19SA010</td>
-            <td>63</td>
+            <td><input type="text"></input></td>
             <td>75</td>
         </tr>
 
