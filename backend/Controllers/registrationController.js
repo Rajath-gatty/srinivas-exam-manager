699a08e3dbd86b6e0fee6e7a689d396f3699e1b8
Rajath-gatty <rajathgatty253@gmail.com>
2022-05-31 17:53:05 +0530

    added faculty and staff registration

--- backend/Controllers/registrationController.js
+++ backend/Controllers/registrationController.js
@@ -1,5 +1,6 @@
 const {validationResult} = require('express-validator');
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
 exports.postStudent = async(req, res) => {
     const data = req.body;
@@ -7,27 +8,46 @@ exports.postStudent = async(req, res) => {
     if(err.length>0) {
        return res.status(400).send({success:false,err});
     }
-    const result = await db.execute('insert into student(regno,first_name,gender,dob,email,phone,address,blood_group,caste,aadhar_no,religion,birth_place,birth_district,country,pincode,password,f_name,f_occupation,f_phone,f_email,m_name,m_occupation,m_phone,m_email,department,course,joining_year) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-    [data.regNo,data.firstName,data.gender,data.dob,data.email,data.phone,data.address,data.bloodGroup,data.caste,data.aadharNo,data.religion,data.birthPlace,data.birthDistrict,data.country,data.pincode,data.password,data.fatherName,data.fatherOccupation,data.fatherPhone,data.fatherEmail,data.motherName,data.motherOccupation,data.motherPhone,data.motherEmail,data.department,data.course,data.joiningYear]);
+    try {
+        const hashedPassword = await bcrypt.hash(data.password,4);
 
-    console.table(result[0]);
-    res.status(200).send({success:true,data});
+        const result = await db.execute('insert into student(regno,first_name,gender,dob,email,phone,address,blood_group,caste,aadhar_no,religion,birth_place,birth_district,country,pincode,password,f_name,f_occupation,f_phone,f_email,m_name,m_occupation,m_phone,m_email,department,course,joining_year,role,status) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+        [data.regNo,data.firstName,data.gender,data.dob,data.email,data.phone,data.address,data.bloodGroup,data.caste,data.aadharNo,data.religion,data.birthPlace,data.birthDistrict,data.country,data.pincode,hashedPassword,data.fatherName,data.fatherOccupation,data.fatherPhone,data.fatherEmail,data.motherName,data.motherOccupation,data.motherPhone,data.motherEmail,data.department,data.course,data.joiningYear,'student','pending']);
+    
+        console.table(result[0]);
+        res.status(200).send({success:true,data});
+    } catch(err) {
+        res.status(500).send(err);
+    }
 }
 
-exports.postFaculty = (req, res) => {
+exports.postFaculty = async(req, res) => {
     const data = req.body;
     const err = validationResult(req).errors;
     if(err.length>0) {
        return res.status(400).send({success:false,err});
     }
-    res.status(200).send({success:true,data});
+    const hashedPassword = await bcrypt.hash(data.password,4);
+
+    const result = await db.execute('insert into faculty(faculty_id,first_name,gender,dob,email,phone,address,blood_group,caste,aadhar_no,religion,birth_place,birth_district,country,pincode,password,f_name,f_occupation,f_phone,f_email,department,joining_year,role,status) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+    [data.facultyId,data.firstName,data.gender,data.dob,data.email,data.phone,data.address,data.bloodGroup,data.caste,data.aadharNo,data.religion,data.birthPlace,data.birthDistrict,data.country,data.pincode,hashedPassword,data.fatherName,data.fatherOccupation,data.fatherPhone,data.fatherEmail,data.department,data.joiningYear,'faculty','pending']);
+    res.status(200).send({success:true,data:result[0]});
 }
 
-exports.postStaff = (req, res) => {
+exports.postStaff = async(req, res) => {
     const data = req.body;
     const err = validationResult(req).errors;
     if(err.length>0) {
        return res.status(400).send({success:false,err});
     }
-    res.status(200).send({success:true,data});
+    const hashedPassword = await bcrypt.hash(data.password,4);
+
+    const result = await db.execute('insert into staff(first_name,gender,dob,email,phone,address,blood_group,caste,aadhar_no,religion,password,role,status) values(?,?,?,?,?,?,?,?,?,?,?,?,?)',
+    [data.firstName,data.gender,data.dob,data.email,data.phone,data.address,data.bloodGroup,data.caste,data.aadharNo,data.religion,hashedPassword,'staff','pending']);
+    res.status(200).send({success:true,data:result[0]});
+}
+
+exports.getStudents = async(req,res) => {
+    const result = await db.execute('select first_name,g_name,joining_year,pincode from student');
+    res.status(200).send({success:true,data:result[0]});
 }
--- backend/Router/registrationRoutes.js
+++ backend/Router/registrationRoutes.js
@@ -32,7 +32,7 @@ body('joiningYear').trim().notEmpty().withMessage('Enter Joining Year')
 
 router.post('/registration/faculty',[
 body('firstName').trim().isLength({min:3}).withMessage('Name must be atleast 3 characters long'),
-body('FacultyId').trim().isLength({min:5}).withMessage('Enter valid Faculty ID'),
+body('facultyId').trim().isLength({min:5}).withMessage('Enter valid Faculty ID'),
 body('gender').trim().isIn(['male','female','others']).withMessage('Please specify Gender'),
 body('dob').trim().isDate({format:'DD/MM/YYYY'}).withMessage('Please Enter Valid Date'),
 body('email').isEmail().withMessage('Enter valid Email ID'),
@@ -66,16 +66,18 @@ body('bloodGroup').trim().notEmpty().withMessage('Enter a valid blood group'),
 body('caste').trim().isLength({min:1}).withMessage('Caste cannot be empty'),
 body('aadharNo').trim().isLength({min:12}).withMessage('Enter valid Aadhar number'),
 body('religion').trim().notEmpty().withMessage('Enter religion'),
-body('birthPlace').trim().notEmpty().withMessage('Birth place cannot be empty'),
-body('birthDistrict').trim().notEmpty().withMessage('Birth District cannot be empty'),
-body('country').trim().notEmpty().withMessage('Country cannot be empty'),
-body('pincode').trim().isLength({min:6}).withMessage('Enter valid pincode'),
+// body('birthPlace').trim().notEmpty().withMessage('Birth place cannot be empty'),
+// body('birthDistrict').trim().notEmpty().withMessage('Birth District cannot be empty'),
+// body('country').trim().notEmpty().withMessage('Country cannot be empty'),
+// body('pincode').trim().isLength({min:6}).withMessage('Enter valid pincode'),
 body('password').trim().isLength({min:5}).withMessage('Password must contain atleast 5 characters'),
-body('fatherName').trim().notEmpty().withMessage('Father name cannot be empty'),
-body('fatherOccupation').trim().notEmpty().withMessage('Father Occupation cannot be empty'),
-body('fatherPhone').trim().isLength({min:10}).withMessage('Enter valid Phone number'),
-body('department').trim().notEmpty().withMessage('Select Department'),
-body('joiningYear').trim().notEmpty().withMessage('Enter Joining Year')
+// body('fatherName').trim().notEmpty().withMessage('Father name cannot be empty'),
+// body('fatherOccupation').trim().notEmpty().withMessage('Father Occupation cannot be empty'),
+// body('fatherPhone').trim().isLength({min:10}).withMessage('Enter valid Phone number'),
+// body('department').trim().notEmpty().withMessage('Select Department'),
+// body('joiningYear').trim().notEmpty().withMessage('Enter Joining Year')
 ],controllers.postStaff);
+
+router.get('/students',controllers.getStudents)
 
 module.exports = router;
