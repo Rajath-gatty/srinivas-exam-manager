3eb93deb072100bd0c1c3cb39fc626d3b177cdd9
ChiragChrg <chiruchirag2001@gmail.com>
2022-05-31 15:57:24 +0530

    Updated Assign Afculty & npm install

--- frontend/src/components/Evaluator/Evaluators/AssignFaculty/AssignFaculty.js
+++ frontend/src/components/Evaluator/Evaluators/AssignFaculty/AssignFaculty.js
@@ -3,7 +3,7 @@ import "./AssignFaculty.css";
 import { FiCheck } from "react-icons/fi";
 import Back from "../../../UI/Back/Back";
 import SelectInput from "../../../UI/SelectInput";
-import { AssignFacultyList } from "./AssignFacultyList";
+// import { AssignFacultyList } from "./AssignFacultyList";
 
 const AssignFaculty = () => {
   var CourseValues = ["BCA", "MCA", "BBA", "MBA", "BCom", "MCom"];
@@ -61,6 +61,7 @@ const AssignFaculty = () => {
   // ];
 
   const [evaluatorOrder, setEvaluatorOrder] = useState({});
+  // const [updatedEvaluator, setUpdatedEvaluator] = useState([]);
   const [assignEvaluator, setAssignEvaluator] = useState([]);
 
   useEffect(() => {
@@ -69,19 +70,9 @@ const AssignFaculty = () => {
 
       //Checking & Updating existing faculty order
       assignEvaluator.some((obj) => {
-        if (obj.user.regno === evaluatorOrder.user.regno) {
+        if (obj.evalReg === evaluatorOrder.evalReg) {
           duplicateFlag = 1;
-          obj.order = evaluatorOrder.order;
-          return;
-        }
-      });
-
-      //Checking if Max 3 orders are occupied
-      assignEvaluator.some((obj) => {
-        //Checking & Updating existing faculty order
-        if (obj.user.regno === evaluatorOrder.user.regno) {
-          duplicateFlag = 1;
-          obj.order = evaluatorOrder.order;
+          obj.evalOrder = evaluatorOrder.evalOrder;
           return;
         }
       });
@@ -91,21 +82,99 @@ const AssignFaculty = () => {
       }
     }
 
+    // console.log(updatedEvaluator.length);
+    // if (updatedEvaluator.length > 0) {
+    //   setAssignEvaluator(updatedEvaluator);
+    //   console.log(updatedEvaluator.length);
+    //   // updatedEvaluator = [];
+    // }
+
     console.table(assignEvaluator);
     FilterOccupiedBtn();
   }, [evaluatorOrder, assignEvaluator]);
 
   // Occupied btn no. sent to AssignFacultyList
   var occupied = [];
+  var orderObj;
+  var orderReg;
   const FilterOccupiedBtn = () => {
     assignEvaluator.forEach((obj) => {
-      var orderObj = obj.order;
-      var orderReg = obj.user.regno;
+      orderObj = obj.evalOrder;
+      orderReg = obj.evalReg;
       occupied.push({ orderObj, orderReg });
     });
   };
 
-  console.log(occupied);
+  const ActiveOrder = (e) => {
+    var currBtn = e.target;
+    var parentBtn = e.target.parentElement;
+
+    var evalOrder = currBtn.value;
+    var evalReg = currBtn.getAttribute("data-id");
+    // var evalReg = parentBtn.getAttribute("data-id");
+
+    // if (currBtn.style.backgroundColor === "var(--primary-color)") {
+    //   currBtn.style.backgroundColor = "var(--light-grey)";
+
+    //   //Delete toggled off Objects
+    //   // assignEvaluator.forEach((obj) => {
+    //   //   if (obj.evalReg === evalReg) {
+    //   //     var UpdatedArr = assignEvaluator.filter(
+    //   //       (rm) => rm.evalReg !== evalReg
+    //   //     );
+    //   //     console.table("---", UpdatedArr);
+    //   //     setUpdatedEvaluator(UpdatedArr);
+    //   //     // setAssignEvaluator(UpdatedArr);
+    //   //   }
+    //   // });
+    // } else {
+    var btnGroup = parentBtn.childNodes;
+    btnGroup.forEach((element) => {
+      element.removeAttribute("style");
+    });
+
+    // setEvaluatorOrder({ evalOrder, evalReg });
+    // currBtn.style.backgroundColor = "var(--primary-color)";
+    // }
+
+    //Column duplicate check
+    var btnOne = document.querySelectorAll("[value='1']");
+    var btnTwo = document.querySelectorAll("[value='2']");
+    var btnThree = document.querySelectorAll("[value='3']");
+
+    btnOne.forEach((btn) => {
+      console.log(btn);
+      var checkStyle = btn.style.backgroundColor;
+      var checkReg = btn.getAttribute("[data-id]");
+
+      occupied.forEach((obj) => {
+        if (
+          checkReg === obj.orderReg &&
+          checkStyle === "var(--primary-color)"
+        ) {
+          //Toggle off selected btn
+          btn.removeAttribute("style");
+          //data is still not deleted from array
+        } else if (checkReg !== obj.orderReg && checkStyle === "") {
+          console.log("Dup Grey");
+          btnOne.forEach((element) => {
+            element.removeAttribute("style");
+          });
+          setEvaluatorOrder({ evalOrder, evalReg });
+          currBtn.style.backgroundColor = "var(--primary-color)";
+        }
+        console.log(checkStyle.length);
+      });
+    });
+
+    // //Column duplicate check
+    // console.log("Goin Here");
+    // if (orderObj === evalOrder && orderReg !== evalReg) {
+    //   console.log("Got Duplicate");
+    // }
+  };
+
+  // console.log(occupied);
   return (
     <div className="assignEvaluator-container">
       <Back />
@@ -135,12 +204,48 @@ const AssignFaculty = () => {
         <tbody>
           {userData.map((data) => {
             return (
-              <AssignFacultyList
-                setEvaluatorOrder={setEvaluatorOrder}
-                info={data}
-                key={data.regno}
-                occupied={occupied}
-              />
+              <tr key={data.regno} className="assignFaculty-row">
+                <td>
+                  <div className="assignFaculty-avatar">
+                    <img
+                      src={data.profile}
+                      alt="Avatar"
+                      width="40px"
+                      height="40px"
+                    />
+                  </div>
+                </td>
+                <td>{data.regno}</td>
+                <td>{data.name}</td>
+                <td>{data.course}</td>
+                <td>{data.semester}</td>
+                <td>{data.bundle}</td>
+                <td>
+                  <div className="EvaluatorOrder">
+                    <button
+                      value="1"
+                      data-id={data.regno}
+                      onClick={ActiveOrder}
+                    >
+                      1
+                    </button>
+                    <button
+                      value="2"
+                      data-id={data.regno}
+                      onClick={ActiveOrder}
+                    >
+                      2
+                    </button>
+                    <button
+                      value="3"
+                      data-id={data.regno}
+                      onClick={ActiveOrder}
+                    >
+                      3
+                    </button>
+                  </div>
+                </td>
+              </tr>
             );
           })}
         </tbody>
