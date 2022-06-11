6a95dfb899c38322279108c1536922b7ed92d7b1
siddharthkm121 <siddharthkm121@gmail.com>
2022-06-11 15:21:38 +0530

    semester marks are updated

---
+++ frontend/src/components/Faculty/SemesterMarks/SemesterMarksList.js
@@ -0,0 +1,19 @@
+import { FaUserCircle } from "react-icons/fa";
+const SemesterMarksList = () => {
+    return (
+        <tr className="internal-table-row">
+            <td className="internal-avatar-wrapper">
+                <FaUserCircle color="var(--light-grey)" size={25} />
+            </td>
+            <td>3SU19SA010</td>
+            <td>John</td>
+            <td>V</td>
+            <td className="internal-input-row"><input type="text"></input></td>
+            <td className="internal-input-row"><input type="text"></input></td>
+            <td className="internal-input-row"><input type="text"></input></td>
+        </tr>
+
+    );
+};
+
+export default SemesterMarksList;
