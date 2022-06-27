e7c3d53c9856826552d67f0b32bc92a04f9a0b10
Rajath-gatty <rajathgatty253@gmail.com>
2022-06-28 01:29:45 +0530

    Co-authored-by: ChiragChrg <ChiragChrg@users.noreply.github.com>

--- frontend/src/components/Faculty/AttendanceFaculty/AttendanceList.js
+++ frontend/src/components/Faculty/AttendanceFaculty/AttendanceList.js
@@ -1,15 +1,22 @@
 import { TextField } from "@mui/material";
 import { FaUserCircle } from "react-icons/fa";
-const AttendanceList = () => {
+
+const AttendanceList = ({data,serverUrl,handleAttendanceChange,handleMarkChange,index}) => {
     return (
         <tr className="attendance-table-row">
             <td className="attendance-avatar-wrapper">
-                <FaUserCircle color="var(--light-grey)" size={25} />
+                {!data.image_path?<FaUserCircle color="var(--light-grey)" size={25} />:
+                <img src={serverUrl+data.image_path} className="profile-img" width="100px"/>}
             </td>
-            <td>3SU19SA010</td>
-            <td>John</td>
-            <td>V</td>
-            <td ><TextField
+            <td>{data.regno}</td>
+            <td>{data.first_name+' '+data.last_name}</td>
+            <td>{data.semester}</td>
+            <td className="attendance-textfield"><TextField
+                onChange={(e)=>{handleAttendanceChange(index,e.target.value)}}
+                variant="outlined"
+                size="small"></TextField></td>
+            <td className="attendance-textfield"><TextField
+            onChange={(e)=>{handleMarkChange(index,e.target.value)}}
                 variant="outlined"
                 size="small"></TextField></td>
 
