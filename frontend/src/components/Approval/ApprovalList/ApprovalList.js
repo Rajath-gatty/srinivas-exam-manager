e83e44e60e9bec3d3851573cf708e2b15ead80c6
Rajath-gatty <rajathgatty253@gmail.com>
2022-07-23 23:03:12 +0530

    Co-authored-by: ChiragChrg <ChiragChrg@users.noreply.github.com>

--- frontend/src/components/Approval/ApprovalList/ApprovalList.js
+++ frontend/src/components/Approval/ApprovalList/ApprovalList.js
@@ -5,26 +5,30 @@ const ApprovaList = (props) => {
  const {name,regno,joiningYear,courseName,type,facultyId,staffId,imageUrl,handleApprove,handleReject} = props;
 
  const {serverUrl} = useContextData();
-  let id;
-  if(type==='student') id=regno;
-  else if(type==='faculty') id=facultyId;
-  else if(type==='staff') id=staffId;
+
+  let UserID = "";
+  if(type === "student")
+    UserID = {uid:regno, idName:"regno"};
+  else if(type === "faculty")
+    UserID = {uid:facultyId, idName:"faculty_id"};
+  else if(type === "staff")
+    UserID = {uid:staffId, idName:"staff_id"};
 
   return (
     <tr className="approve-table-row">
       {type==='student'&&<td className="approve-avatar-wrapper">
-      <img src={serverUrl+imageUrl} className="approval-profile-img" alt="" />
+      <img src={serverUrl+imageUrl} className="approval-profile-img" alt="profile" width="40px" height="40px"/>
       </td>}
       <td>{name}</td>
-      {type==='student'&&<td>{regno}</td>}
+      {type==='student'&&<td>{UserID.uid}</td>}
       {type==='student'&&<td>{courseName}</td>}
-      <td>{joiningYear}</td>
+      <td>{joiningYear}</td> 
       <td>
-        <Link to={`/approve/${type}/${id}?type=${type}`}>View</Link>
+        <Link to={`/approve/${type}/${UserID.uid}`} state={{type:type, userId:UserID }}>View</Link>
       </td>
       <td className="flex approve-table-btn-wrapper">
-        <button onClick={()=> handleApprove(id)} className="btn-outlined-green">Approve</button>
-        <button onClick={()=> handleReject(id,imageUrl)}  className="btn-outlined-red">Reject</button>
+        <button onClick={()=> handleApprove(UserID.uid)} className="btn-outlined-green">Approve</button>
+        <button onClick={()=> handleReject(UserID.uid,imageUrl)}  className="btn-outlined-red">Reject</button>
       </td>
     </tr>
   );
