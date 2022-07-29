f14075dc0ca7c02eb8b8b60b83e276d148d1d4da
ChiragChrg <chiruchirag2001@gmail.com>
2022-07-29 20:29:27 +0530

    Mobile Responsive PageNotFound

--- frontend/src/pages/PageNotFound/PageNotFound.js
+++ frontend/src/pages/PageNotFound/PageNotFound.js
@@ -2,27 +2,18 @@ import "./PageNotFound.css";
 import SrinivasLogo from "../../Assets/SrinivasLogo.svg";
 import NotFound from "../../Assets/notFound.gif";
 import {Link} from "react-router-dom";
-import {FiArrowLeft} from "react-icons/fi"
+import {FiArrowLeft} from "react-icons/fi";
+import Navbar from "../../components/Navbar/Navbar";
+
 const PageNotFound = () => {
     return (
         <div className="notFound-container flex">
-            <div className="notFound-logo flex">
-                <img width="50px" src={SrinivasLogo} alt="Login SVG" />
-                <h1>Srinivas Exam Manager</h1>
-            </div>
-
-            <div className="notFound-back">
-                <Link to="/" className="flex">
-                    <FiArrowLeft size={25}/>
-                    <span>Home</span>
-                </Link>
-            </div>
+            <Navbar />
 
             <div className="notFound-Img flex">
-                <img src={NotFound} alt="Page NotFound" width="550px" />
+                <img src={NotFound} alt="Page NotFound" width="500px" />
                 <h3>Page not Found</h3>
             </div>
-            
         </div>    
     )
 }
