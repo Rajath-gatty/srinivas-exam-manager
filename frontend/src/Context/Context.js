d278edb7358f631866be49607f73abc97384c6b5
Rajath-gatty <rajathgatty253@gmail.com>
2022-08-19 20:32:58 +0530

    url fix

--- frontend/src/Context/Context.js
+++ frontend/src/Context/Context.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 export const Context = React.createContext();
 
 const ContextProvider = (props) => {
-  // const [serverUrl] = useState('https://exam-manager-backend.herokuapp.com');
-  const [serverUrl] = useState('http://localhost:8080');
+  const [serverUrl] = useState('https://exam-manager-backend.herokuapp.com');
+  // const [serverUrl] = useState('http://localhost:8080');
   const [role, setRole] = useState('');
   const [user, setUser] = useState({});
   const [token, setToken] = useState('');
