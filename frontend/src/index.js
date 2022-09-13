37f7cfaa3519d2a9d0ad5697ee26fe32196aedbd
Rajath-gatty <rajathgatty253@gmail.com>
2022-09-13 13:28:45 +0530

    url fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,11 @@ import App from './App';
 import ContextProvider from "./Context/Context";
 import axios from "axios";
 
-// axios.defaults.baseURL = 'https://exam-manager-backend.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://exam-manager-backend.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:8080';
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(<ContextProvider><App /></ContextProvider>);
+ 
