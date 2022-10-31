965b956a8e10bff5a4fe69b28daa1c53eb75e50f
ChiragChrg <chiruchirag2001@gmail.com>
2022-10-31 21:09:16 +0530

    PWA Toast Update

--- frontend/src/util/Pwa.js
+++ frontend/src/util/Pwa.js
@@ -41,8 +41,8 @@ const Pwa = () => {
         deferredPrompt = null;
         console.log("PWA was installed");
         toast.success("PWA installed 👍", {
-            position: "top-right",
-            autoClose: 8000,
+            position: "bottom-center",
+            autoClose: 5000,
         });
     });
 }
