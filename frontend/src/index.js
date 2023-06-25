90494d6b580c719507b9c6525dca4f2cabb137ef
Rajath-gatty <rajathgatty253@gmail.com>
2023-06-25 08:44:52 +0530

    backend url changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import ContextProvider from "./Context/Context";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://srinivas-exam-manager.onrender.com';
+axios.defaults.baseURL = 'https://fl7d9p8gpi.execute-api.ap-south-1.amazonaws.com/prod';
 // axios.defaults.baseURL = 'http://localhost:8080';
-
+//Backup url - 'https://srinivas-exam-manager.onrender.com';
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
